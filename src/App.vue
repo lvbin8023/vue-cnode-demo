2c52fa8a0d01283d2faf714a9d65c78eeccefefe
lvbin <472245767@qq.com>
2019-02-26 16:42:12 +0800

    cnode社区代码

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <PostList></PostList>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import PostList from './components/PostList';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    PostList
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+*::before {
+  margin: 0;
+  padding: 0;
+}
+
+*::after {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  background-color: #e1e1e1;
+}
+
+.main {
+  width: 67%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt>
+    <ul>
+      <li>
+        <a href>首页</a>
+      </li>
+      <li>
+        <a href>新手入门</a>
+      </li>
+      <li>
+        <a href>API</a>
+      </li>
+      <li>
+        <a href>关于</a>
+      </li>
+      <li>
+        <a href>注册</a>
+      </li>
+      <li>
+        <a href>登录</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
+
+<style scoped>
+.header {
+  background: #444;
+  height: 50px;
+}
+
+img {
+  max-width: 120px;
+  margin-top: 10px;
+  margin-left: 50px;
+}
+
+ul {
+  float: right;
+  right: 0;
+  list-style: none;
+  margin: 4px;
+}
+
+li {
+  display: inline-block;
+  padding: 10px 15px;
+}
+
+a {
+  text-decoration: none;
+  color: #ccc;
+  font-size: 14px;
+  text-shadow: none;
+}
+</style>
